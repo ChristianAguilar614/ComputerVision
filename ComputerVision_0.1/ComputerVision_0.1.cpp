fb11cf2473ff994754664c8b748ca0cef2359779
Justin Oroz <justin.oroz@me.com>
2017-02-03 19:33:39 -0800

    added checks for proper camera initialization added forced camera frame dimensions added checks for empty frames

--- ComputerVision_0.1/ComputerVision_0.1.cpp
+++ ComputerVision_0.1/ComputerVision_0.1.cpp
@@ -7,31 +7,51 @@
 // Christian Aguilar
 
 #include <iostream>
-#include "stdafx.h"
+	//#include "stdafx.h"
 #include <stdio.h>
 
-#include "opencv2\opencv.hpp"
-#include "opencv2\highgui\highgui.hpp"
-#include "opencv2\objdetect\objdetect.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
 
 using namespace cv;
+using namespace std;
 
 int main(int, char**)
 {
+
 	VideoCapture cam(0); // open Camera #1
-						 //VideoCapture cam2(1);// open Camera #2
-	if (!cam.isOpened()) return 1;
-	//if(!cam2.isOpened()) return 1;
+	//VideoCapture cam2(1);// open Camera #2
+
+	// set video capture properties for MacBook' iSight camera
+	cam.set(CV_CAP_PROP_FRAME_WIDTH, 500);
+	cam.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
+
+	if( !cam.isOpened() )
+	{
+		cerr << "***Could not initialize capturing...***\n";
+		cerr << "Current parameter's value: \n";
+		return -1;
+	}
+
 
 	Mat edges;
 	Mat3b frame0;
 	for (;;)
 	{
 		cam >> frame0;
+
+		if(frame0.empty()){
+			cerr << "frame is empty" << endl;
+			break;
+		}
+
+
 		cvtColor(frame0, edges, CV_BGR2GRAY); //to Gray image
 		GaussianBlur(edges, edges, Size(3, 3), 1.5, 1.5); //Reduce noise
 		Canny(edges, edges, 10, 70, 3); //Find edges and ommit everything else
-		
+
 		//show both raw and edges
 		imshow("Edges", edges);
 		imshow("Raw", frame0);
